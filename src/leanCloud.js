371eb915089008e90bbd3620088adb6e1f886d8b
101piano <771594535@qq.com>
2017-05-18 20:10:33 +0800

    add leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+
+import AV from 'leancloud-storage';
+var APP_ID = 'LzQW0DEPEdxCqtgo9ePKkzsm-gzGzoHsz';
+var APP_KEY = 'F4tJ4JqjmUChkRhLNsAbt99r';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
